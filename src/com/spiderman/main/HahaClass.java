97ad6538c6fbe06d0c5916bdbfc715af49787d9b
csp <1187814176@qq.com>
2018-11-12 15:59:18 +0800

    2222

--- src/com/spiderman/main/HahaClass.java
+++
@@ -1,6 +0,0 @@
-package com.spiderman.main;
-
-public class HahaClass {
-	//2322222
-	//00000
-}
--- src/com/spiderman/main/MyClass.java
+++
@@ -1,7 +0,0 @@
-package com.spiderman.main;
-
-public class MyClass {
-	public static void main(String[] args){
-		/////123
-	}
-}
